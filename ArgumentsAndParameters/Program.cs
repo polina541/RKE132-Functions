f7b62678b419fede8e8b0606ee64d321495019e6
Polina Smm. <polina.somova@tktk.ee>
2024-03-03 13:02:36 +0200

    add functions code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter a word");
+string userWord = Console.ReadLine();
+
+PrintAnyWord(userWord); 
+
+static void PrintAnyWord(string anyString)
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i ++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
